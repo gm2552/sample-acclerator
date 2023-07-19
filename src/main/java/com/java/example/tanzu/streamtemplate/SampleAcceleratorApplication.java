f46fd218ea7824bad766e524d3625b8b67cdd2bf
gm2552 <meyerg@vmware.com>
2023-07-19 09:24:21 -0500

    Initial checkin of application.

---
+++ src/main/java/com/java/example/tanzu/streamtemplate/SampleAcceleratorApplication.java
@@ -0,0 +1,13 @@
+package com.java.example.tanzu.streamtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAcceleratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAcceleratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/example/tanzu/streamtemplate/SampleAcceleratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.example.tanzu.streamtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAcceleratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
